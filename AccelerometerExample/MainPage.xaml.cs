eddd2729aeb10c02ce7cbd9239b807c62619c626
Fiyaz Bin Hasan <fiyazhasan@live.com>
2014-02-04 21:10:03 +0600

    2nd commit
    camera opens when in landscap right with range x between 1-0.90 and in
    landscap left with x between -1.00 - -0.90...and else it will show the
    map.

--- AccelerometerExample/MainPage.xaml.cs
+++ AccelerometerExample/MainPage.xaml.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
 using System.Windows;
-using System.Windows.Controls;
+using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
+using Coding4Fun.Toolkit.Controls;
 using Microsoft.Phone.Controls;
-using Microsoft.Phone.Shell;
-using AccelerometerExample.Resources;
 using Microsoft.Devices.Sensors;
 using Microsoft.Phone.Tasks;
 using Microsoft.Xna.Framework;
@@ -19,95 +15,94 @@ namespace AccelerometerExample
         // Constructor
         Accelerometer accelerometer;
         CameraCaptureTask cameraCaptureTask;
+        private Vector3 _acceleration;
         public MainPage()
         {
             InitializeComponent();
-            if (!Accelerometer.IsSupported)
-            {
-                // The device on which the application is running does not support
-                // the accelerometer sensor. Alert the user and disable the
-                // Start and Stop buttons.
-                statusTextBlock.Text = "device does not support accelerometer";
-            }
+
             // Sample code to localize the ApplicationBar
             //BuildLocalizedApplicationBar();
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            if (accelerometer == null)
+            if (Accelerometer.IsSupported)
             {
-                // Instantiate the Accelerometer.
-                accelerometer = new Accelerometer();
-                accelerometer.TimeBetweenUpdates = TimeSpan.FromMilliseconds(20);
-                accelerometer.CurrentValueChanged +=
-                    accelerometer_CurrentValueChanged;
+                if (accelerometer == null)
+                {
+                    accelerometer = new Accelerometer();
+                    accelerometer.Start();
+                    accelerometer.TimeBetweenUpdates = TimeSpan.FromMilliseconds(20);
+                    accelerometer.CurrentValueChanged += accelerometer_CurrentValueChanged;
+                }
+                else
+                {
+                    accelerometer.Start();
+                    accelerometer.TimeBetweenUpdates = TimeSpan.FromMilliseconds(20);
+                    accelerometer.CurrentValueChanged += accelerometer_CurrentValueChanged;
+                }
             }
-            try
-            {
-                statusTextBlock.Text = "starting accelerometer.";
-                accelerometer.Start();
-            }
-            catch (InvalidOperationException ex)
-            {
-                statusTextBlock.Text = "unable to start accelerometer.";
-            }
+
         }
 
         void accelerometer_CurrentValueChanged(object sender, SensorReadingEventArgs<AccelerometerReading> e)
         {
+
             // Call UpdateUI on the UI thread and pass the AccelerometerReading.
-            Dispatcher.BeginInvoke(() => UpdateUI(e.SensorReading));
-        }
-
-        private void UpdateUI(AccelerometerReading accelerometerReading)
-        {
-            statusTextBlock.Text = "getting data";
-
-            Vector3 acceleration = accelerometerReading.Acceleration;
-
-            // Show the numeric values.
-            xTextBlock.Text = "X: " + acceleration.X.ToString("0.00");
-            yTextBlock.Text = "Y: " + acceleration.Y.ToString("0.00");
-            zTextBlock.Text = "Z: " + acceleration.Z.ToString("0.00");
-
-
-            //// Show the values graphically.
-            //xLine.X2 = xLine.X1 + acceleration.X * 200;
-            //yLine.Y2 = yLine.Y1 - acceleration.Y * 200;
-            //zLine.X2 = zLine.X1 - acceleration.Z * 100;
-            //zLine.Y2 = zLine.Y1 + acceleration.Z * 100;
-
-            if (acceleration.Y.ToString("0.00") == "-1.00")
+            if (e.SensorReading.Acceleration.X <= -0.90)
             {
-                accelerometer.CurrentValueChanged -= accelerometer_CurrentValueChanged;
                 cameraCaptureTask = new CameraCaptureTask();
                 cameraCaptureTask.Show();
+                accelerometer.Stop();
                 cameraCaptureTask.Completed += cameraCaptureTask_Completed;
             }
-            else if (acceleration.Y >= -1.00)
+            if(e.SensorReading.Acceleration.X >= 0.90)
             {
-                Map.Visibility = Visibility.Visible;
+                cameraCaptureTask = new CameraCaptureTask();
+                cameraCaptureTask.Show();
+                accelerometer.Stop();
+                cameraCaptureTask.Completed += cameraCaptureTask_Completed;
             }
-            else
-            {
-                Map.Visibility = Visibility.Collapsed;
-            }
+            Dispatcher.BeginInvoke(() => UpdateTextBoxes(e.SensorReading.Acceleration));
+        }
+
+        private void UpdateTextBoxes(Vector3 acceleration)
+        {
+            TextBlock.Text = acceleration.X.ToString();
         }
 
         void cameraCaptureTask_Completed(object sender, PhotoResult e)
         {
             if (e.TaskResult == TaskResult.OK)
             {
-                MessageBox.Show(e.ChosenPhoto.Length.ToString());
-                accelerometer.CurrentValueChanged += accelerometer_CurrentValueChanged;
-
-                //Code to display the photo on the page in an image control named myImage.
-                //System.Windows.Media.Imaging.BitmapImage bmp = new System.Windows.Media.Imaging.BitmapImage();
-                //bmp.SetSource(e.ChosenPhoto);
-                //myImage.Source = bmp;
+                MessagePrompt prompt = new MessagePrompt();
+                prompt.Message = "Photo saved in " + e.OriginalFileName;
+                prompt.HorizontalAlignment = HorizontalAlignment.Center;
+                prompt.VerticalAlignment = VerticalAlignment.Center;
+                prompt.Show();
             }
         }
+
+        private void PhoneApplicationPage_OrientationChanged(object sender, OrientationChangedEventArgs e)
+        {
+            if ((e.Orientation & PageOrientation.LandscapeLeft) == PageOrientation.LandscapeLeft)
+            {
+                accelerometer.CurrentValueChanged += accelerometer_CurrentValueChanged;
+            }
+            else if ((e.Orientation & PageOrientation.LandscapeRight) == PageOrientation.LandscapeRight)
+            {
+                accelerometer.CurrentValueChanged += accelerometer_CurrentValueChanged;
+            }
+            else
+            {
+                accelerometer.Start();
+                accelerometer.TimeBetweenUpdates = TimeSpan.FromMilliseconds(20);
+                accelerometer.CurrentValueChanged += accelerometer_CurrentValueChanged;
+            }
+        }
+
+
+
 
 
 
